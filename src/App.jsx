b83603cc975dfed00912da89b0365550e5873bdb
codingghoul7 <the.raushankr@gmail.com>
2025-09-13 06:05:53 +0530

    Added dashboard and dropDown functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
 
+
 function App() {
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { FaBarsStaggered } from "react-icons/fa6";
@@ -6,8 +6,32 @@ import { FaSearch } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 import { FaShoppingCart } from "react-icons/fa";
+import user from "../assets/user.jpg";
+
+const navigation = [
+    {
+        name: "Dashboard", href: "/dashboard"
+    },
+    {
+        name: "Orders", href: "/orders"
+    },
+    {
+        name: "Cart Page", href: "/cart-page"
+    },
+    {
+        name: "Logout", href: "/logout"
+    }
+]
+
 
 export default function Navbar() {
+
+    const currentUser = true;
+    const [isdropDownOpen, setisdropDownOpen] = useState(false);
+    console.log(isdropDownOpen)
+
+
+
     return (
         <header className="max-w-screen-2xl mx-auto px-4 py-6">
             <nav className='flex justify-around items-center'>
@@ -16,7 +40,7 @@ export default function Navbar() {
                     <Link to="/"><FaBarsStaggered className='size-6' /></Link>
 
                     <div className='relative sm:w-72 w-40 space:x-2'>
-                        <FaSearch className='absolute inline-block  left-2 inset-y-2' />
+                        <FaSearch className='absolute inline-block   left-2 inset-y-2' />
                         <input type="text" placeholder='Search' className='border bg-[#EAEAEA] w-full  rounded-md px-6 py-1 md:px-8 focus:outline-none  ' />
                     </div>
                 </div>
@@ -25,7 +49,29 @@ export default function Navbar() {
 
                 {/*right side */}
                 <div className='relative flex items-center sm:space-x-3 space-2 text-2xl'>
-                    <CiUser className='size-6' />
+                    <div>
+                        {
+                            currentUser ?
+                                <><button onClick={() => { setisdropDownOpen(!isdropDownOpen) }}><img src={user} alt="" className={`size-7 rounded-full my-1
+                                ${currentUser ? "ring-2 ring-yellow-500" : ""}
+                                `} /></button>
+                                    {/* Show drodowns*/}
+                                    {isdropDownOpen && (
+                                        <div className='absolute mt-2 w-48 right-0 bg-white shadow-lg rounded-md  z-40 '>
+                                            <ul className='py-2'>
+                                                {navigation.map((item) => (
+                                                    <li key={item.name} onClick={() => setisdropDownOpen(false)}>
+                                                        <Link to={item.href} className='block px-4 py-2 text-sm hover:bg-grey-100'>{item.name}</Link>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+                                </>
+                                : <Link to="/">  <CiUser className='size-6' /></Link>
+                        }
+                    </div>
+
                     <CiHeart className='size-6' />
                     <button className='hidden sm-block'><CiHeart className='size-6' /></button>
                     <Link to="/cart" className='bg-secondary 
