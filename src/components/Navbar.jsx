ecd4191e9afc7de3712928bcc2c267bbd8736f23
Brihaspati <108980953+brihaspati912@users.noreply.github.com>
2026-01-21 14:14:15 +0530

    Adjust Navbar input width for responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,12 +50,12 @@ export default function Navbar() {
                 <div className='flex items-center md:gap-16 gap-4'>
                     <Link to="/"><FaBarsStaggered className='size-7' /></Link>
 
-                   <div className="relative w-full sm:max-w-xs">
+                 <div className="relative w-full sm:w-72">
   <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
   <input
     type="text"
     placeholder="Search"
-    className="w-full pl-10 pr-4 py-2 rounded-md border bg-[#EAEAEA] focus:outline-none focus:ring-2 focus:ring-yellow-500"
+    className="w-full pl-10 pr-4 py-2 rounded-md border bg-[#EAEAEA] focus:outline-none focus:ring-2 focus:ring-yellow-500 text-sm sm:text-base"
   />
 </div>
                 </div>
