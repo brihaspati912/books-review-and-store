11f74b0f3a9974b002bcc303414cf90cbcb62389
Brihaspati <108980953+brihaspati912@users.noreply.github.com>
2026-01-21 14:17:34 +0530

    Adjust padding for search input in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
   <input
     type="text"
     placeholder="Search"
-    className="w-full pl-10 pr-4 py-2 rounded-md border bg-[#EAEAEA] focus:outline-none focus:ring-2 focus:ring-yellow-500 text-sm sm:text-base"
+    className="w-full sm:pl-8 pl-10 pr-4 py-2 rounded-md border bg-[#EAEAEA] focus:outline-none focus:ring-2 focus:ring-yellow-500 text-sm sm:text-base"
   />
 </div>
                 </div>
