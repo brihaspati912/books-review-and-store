f877d6aa372b4bb35168ad45100ad952abc7f4af
Brihaspati <108980953+brihaspati912@users.noreply.github.com>
2026-01-21 14:52:57 +0530

    Update getBaseUrl to use environment variable first

--- src/utilis/baseUrl.js
+++ src/utilis/baseUrl.js
@@ -1,4 +1,4 @@
 const getBaseUrl = () => {
-    return "https://book-review-and-store-backend.vercel.app" || import.meta.env.VITE_BASE_URL;
+     return import.meta.env.VITE_BASE_URL || "http://localhost:5000";
 }
 export default getBaseUrl;
